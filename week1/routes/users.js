099c793b08b35efffb04538e0bf358a63f3db9c7
tovertractor <as.ansano@student.avans.nl>
2024-06-12 18:25:06 +0200

    removing testing variable

--- week1/routes/users.js
+++ week1/routes/users.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 const { db } = require('../services/database');
-var unused = ""
 
 /* GET users listing. */
 router.get('/', async function(req, res) {
