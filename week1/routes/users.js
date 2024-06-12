8b416b6f967f278184a1c91d07124b0af5adce0f
tovertractor <as.ansano@student.avans.nl>
2024-06-12 18:23:45 +0200

    adding test var to test linter

--- week1/routes/users.js
+++ week1/routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const { db } = require('../services/database');
+var unused = ""
 
 /* GET users listing. */
 router.get('/', async function(req, res) {
