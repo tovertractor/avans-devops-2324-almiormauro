fbd314382b6f89df0c02d43952a2a98b571c68e8
tovertractor <as.ansano@student.avans.nl>
2024-06-12 21:22:53 +0200

    setting up basic docker compose

--- week1/services/database.js
+++ week1/services/database.js
@@ -3,6 +3,10 @@ const { MongoClient } = require("mongodb")
 
 const uri = process.env.MONGO_URL
 
+console.log("_________________________________________")
+console.log(uri)
+console.log("_________________________________________")
+
 const client = new MongoClient(uri)
 
 const db = client.db(process.env.DB_NAME)
