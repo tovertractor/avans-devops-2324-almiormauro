a701d38267846d778570d3a3fdf660a62efd0943
tovertractor <as.ansano@student.avans.nl>
2024-06-12 23:21:39 +0200

    remove logging

--- week1/services/database.js
+++ week1/services/database.js
@@ -3,9 +3,6 @@ const { MongoClient } = require("mongodb")
 
 const uri = process.env.MONGO_URL
 
-console.log("_________________________________________")
-console.log(uri)
-
 const client = new MongoClient(uri)
 
 const db = client.db(process.env.DB_NAME)
