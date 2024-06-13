e74c935d47d01e6c583186bc20be0cedefa6560c
tovertractor <as.ansano@student.avans.nl>
2024-06-13 15:28:15 +0200

    prometheus works without grafana

--- week1/app.js
+++ week1/app.js
@@ -7,6 +7,16 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const promBundle = require('express-prom-bundle');
+const metricsMiddleware = promBundle({
+  includePath: true,
+  includeStatusCode: true,
+  normalizePath: true,
+  promClient: {
+    collectDefaultMetrics: {}
+  }
+})
+
 var app = express();
 
 // view engine setup
@@ -18,6 +28,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join('public')));
+
+app.use(metricsMiddleware)
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- week1/routes/users.js
+++ week1/routes/users.js
@@ -1,15 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-const { db } = require('../services/database');
+const { db, client} = require('../services/database');
+
+const prom = require("prom-client")
+const guage = new prom.Gauge({ name: 'number_requests', help: "number of requests send to user service"})
 
 /* GET users listing. */
 router.get('/', async function(req, res) {
+    guage.inc()
     let users = await db.collection('users').find().toArray();
     res.json(users);
 });
 
 router.post('/', function(req, res) {
+    guage.inc()
     db.collection('users').insertOne(req.body)
         .then((user) => res.status(201).json({ "id": user.insertedId }))
         .catch(err => res.status(500).json(err));
