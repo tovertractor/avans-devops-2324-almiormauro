561c4b4173aa4c4629b2fa05fa7f81e125d81f31
tovertractor <as.ansano@student.avans.nl>
2024-06-14 10:25:30 +0200

    creating app 2

---
+++ app2/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(express.json());
+
+const indexRoutes = require("./routes/index")
+
+app.use('/', indexRoutes)
+
+app.listen(PORT, () =>{
+    console.log(`app2 server started on port ${PORT}`);
+})
