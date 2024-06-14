715500af316c81580eedd8b12f57b7a78ac20ce6
tovertractor <as.ansano@student.avans.nl>
2024-06-14 14:16:18 +0200

    fixing prometheus with 2 apps

--- app2/index.js
+++ app2/index.js
@@ -1,10 +1,23 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+var logger = require('morgan');
 
 app.use(express.json());
+app.use(logger('dev'));
 
 const indexRoutes = require("./routes/index")
+
+const promBundle = require('express-prom-bundle');
+const metricsMiddleware = promBundle({
+    includePath: true,
+    includeStatusCode: true,
+    normalizePath: true,
+    promClient: {
+        collectDefaultMetrics: {}
+    }
+})
+app.use(metricsMiddleware)
 
 app.use('/', indexRoutes)
 
